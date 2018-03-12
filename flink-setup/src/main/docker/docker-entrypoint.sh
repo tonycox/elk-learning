e47ba110667b84af472112df920bd291ba04235b
Anton Solovev <tonycoxbk@gmail.com>
2018-03-12 05:54:23 +0400

    minor refactoring in scripts

--- flink-setup/src/main/docker/docker-entrypoint.sh
+++ flink-setup/src/main/docker/docker-entrypoint.sh
@@ -2,6 +2,7 @@
 
 # If unspecified, the hostname of the container is taken as the JobManager address
 JOB_MANAGER_RPC_ADDRESS=${JOB_MANAGER_RPC_ADDRESS:-$(hostname -f)}
+FLINK_HOME=${FLINK_HOME:/opt/flink}
 
 drop_privs_cmd() {
     if [ -x /sbin/su-exec ]; then
@@ -16,7 +17,7 @@ drop_privs_cmd() {
 if [ "$1" = "help" ]; then
     echo "Usage: $(basename "$0") (jobmanager|taskmanager|local|help)"
     exit 0
-if [ "$1" = "jobmanager" ]; then
+elif [ "$1" = "jobmanager" ]; then
     echo "Starting Job Manager"
     sed -i -e "s/jobmanager.rpc.address: localhost/jobmanager.rpc.address: ${JOB_MANAGER_RPC_ADDRESS}/g" "$FLINK_HOME/conf/flink-conf.yaml"
     echo "blob.server.port: 6124" >> "$FLINK_HOME/conf/flink-conf.yaml"
